b7eeb40fd00fc61df98c996a8e4d77e67e23e01a
Qida Ye <q.ye@tu-braunschweig.de>
2023-09-07 21:20:12 +0200

    formatted Diamond.java

--- src/main/java/codo/Diamond.java
+++ src/main/java/codo/Diamond.java
@@ -2,29 +2,26 @@ package codo;
 
 public class Diamond {
     public static String print(int size) {
+
         if (size % 2 == 0 || size < 0) {
             return null;
         }
 
-
         StringBuilder diamond = new StringBuilder();
         int middle = size / 2;
-
-        for (int layer=0; layer<middle; layer++) {
-                diamond.append(" ".repeat(middle-layer));
-                diamond.append("*".repeat(1+layer*2));
+        for (int i=0; i<middle; i++) {
+                diamond.append(" ".repeat(middle-i));
+                diamond.append("*".repeat(1+i*2));
                 diamond.append("\n");
         }
-        diamond.append("*".repeat(size));
-        diamond.append("\n");
-        String[] diamondLayers = diamond.toString().split("\n");
 
+        diamond.append("*".repeat(size)).append("\n");
+
+        String[] diamondLayers = diamond.toString().split("\n");
         for (int i=diamondLayers.length-2; i>=0; i--) {
             diamond.append(diamondLayers[i]).append("\n");
         }
-        // TODO your code here
         return diamond.toString();
     }
-
 
 }
